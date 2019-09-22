b334c7d5b7e47dea39f0e0a4de96fae0303d53ca
sky-big <sky_big@yeah.net>
2019-09-22 05:06:40 +0000

    add build pulsar operator image and push to dockerhub

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+IMAGE=skybig/pulsar-operator:latest
+
+# build operator
+cd .. && make build && cd ./docker
+
+# get pulsar oprator bin
+cp ../bin/pulsar-operator .
+
 echo "[START] build pulsar operator images"
 
+# build docker image
+docker build --tag "${IMAGE}" .
+
+# push docker image
+docker push "${IMAGE}"
+
 echo "[END] build pulsar operator images"
+
+# remove pulsar operator bin
+rm -f ./pulsar-operator
