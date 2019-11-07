1e6f43fb25baaa2ad548a65c38edb23c161f152f
sky-big <sky_big@yeah.net>
2019-11-07 16:09:37 +0800

    update prometheus config map

--- pkg/components/monitor/prometheus/configmap.go
+++ pkg/components/monitor/prometheus/configmap.go
@@ -32,36 +32,36 @@ func MakeConfigMapName(c *pulsarv1alpha1.PulsarCluster) string {
 var prometheusConfig = `
 global:
   scrape_interval: 15s
-  scrape_configs:
-    - job_name: 'prometheus'
-      static_configs:
-        - targets: ['localhost:9090']
+scrape_configs:
+  - job_name: 'prometheus'
+    static_configs:
+      - targets: ['localhost:9090']
+  - job_name: 'kubernetes-pods'
+    kubernetes_sd_configs:
+      - role: pod
 
-    - job_name: 'kubernetes-pods'
-      kubernetes_sd_configs:
-        - role: pod
-      relabel_configs:
-        - source_labels: [__meta_kubernetes_pod_annotation_prometheus_io_scrape]
-          action: keep
-          regex: true
-        - source_labels: [__meta_kubernetes_pod_annotation_prometheus_io_path]
-          action: replace
-          target_label: __metrics_path__
-          regex: (.+)
-        - source_labels: [__address__, __meta_kubernetes_pod_annotation_prometheus_io_port]
-          action: replace
-          regex: ([^:]+)(?::\d+)?;(\d+)
-          replacement: $1:$2
-          target_label: __address__
-        - action: labelmap
-          regex: __meta_kubernetes_pod_label_(.+)
-        - source_labels: [__meta_kubernetes_namespace]
-          action: replace
-          target_label: kubernetes_namespace
-        - source_labels: [__meta_kubernetes_pod_label_component]
-          action: replace
-          target_label: job
-        - source_labels: [__meta_kubernetes_pod_name]
-          action: replace
-          target_label: kubernetes_pod_name
+    relabel_configs:
+      - source_labels: [__meta_kubernetes_pod_annotation_prometheus_io_scrape]
+        action: keep
+        regex: true
+      - source_labels: [__meta_kubernetes_pod_annotation_prometheus_io_path]
+        action: replace
+        target_label: __metrics_path__
+		regex: (.+)
+      - source_labels: [__address__, __meta_kubernetes_pod_annotation_prometheus_io_port]
+		action: replace
+		regex: ([^:]+)(?::\d+)?;(\d+)
+		replacement: $1:$2
+		target_label: __address__
+      - action: labelmap
+		regex: __meta_kubernetes_pod_label_(.+)
+      - source_labels: [__meta_kubernetes_namespace]
+		action: replace
+		target_label: kubernetes_namespace
+	  - source_labels: [__meta_kubernetes_pod_label_component]
+		action: replace
+		target_label: job
+	  - source_labels: [__meta_kubernetes_pod_name]
+		action: replace
+		target_label: kubernetes_pod_name
 `
