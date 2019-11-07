716bfdb4f910a039ad51cce3cbd61051a182db7b
sky-big <sky_big@yeah.net>
2019-11-07 17:05:24 +0800

    update prometheus configmap

--- pkg/components/monitor/prometheus/configmap.go
+++ pkg/components/monitor/prometheus/configmap.go
@@ -47,21 +47,21 @@ scrape_configs:
       - source_labels: [__meta_kubernetes_pod_annotation_prometheus_io_path]
         action: replace
         target_label: __metrics_path__
-		regex: (.+)
+        regex: (.+)
       - source_labels: [__address__, __meta_kubernetes_pod_annotation_prometheus_io_port]
-		action: replace
-		regex: ([^:]+)(?::\d+)?;(\d+)
-		replacement: $1:$2
-		target_label: __address__
+        action: replace
+        regex: ([^:]+)(?::\d+)?;(\d+)
+        replacement: $1:$2
+        target_label: __address__
       - action: labelmap
-		regex: __meta_kubernetes_pod_label_(.+)
+        regex: __meta_kubernetes_pod_label_(.+)
       - source_labels: [__meta_kubernetes_namespace]
-		action: replace
-		target_label: kubernetes_namespace
-	  - source_labels: [__meta_kubernetes_pod_label_component]
-		action: replace
-		target_label: job
-	  - source_labels: [__meta_kubernetes_pod_name]
-		action: replace
-		target_label: kubernetes_pod_name
+        action: replace
+        target_label: kubernetes_namespace
+      - source_labels: [__meta_kubernetes_pod_label_component]
+        action: replace
+        target_label: job
+      - source_labels: [__meta_kubernetes_pod_name]
+        action: replace
+        target_label: kubernetes_pod_name
 `
