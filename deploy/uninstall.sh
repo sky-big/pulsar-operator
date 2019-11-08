041cf71a2f3008641c9e86da73a0218f0b93b38e
sky-big <sky_big@yeah.net>
2019-11-08 18:16:35 +0800

    update install script for rbac

--- deploy/uninstall.sh
+++ deploy/uninstall.sh
@@ -6,7 +6,7 @@ export WORK_DIR=$(cd `dirname $0`; pwd)
 kubectl delete -f ${WORK_DIR}/operator.yaml
 
 # delete pulsar cluster operator account and role
-kubectl delete -f ${WORK_DIR}/rbac/default_namespace_rbac.yaml
+kubectl delete -f ${WORK_DIR}/rbac/all_namespace_rbac.yaml
 
 # delete pulsar cluster crd
 kubectl delete -f ${WORK_DIR}/crds/pulsar.apache.org_pulsarclusters_crd.yaml
